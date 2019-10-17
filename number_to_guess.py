95df65dedc14a4831c10629765263f07c4cd070d
kabp11 <56281549+kabp11@users.noreply.github.com>
2019-10-17 16:07:12 -0500

    Add files via upload

---
+++ number_to_guess.py
@@ -0,0 +1,12 @@
+number_to_guess_input = {}.format(what_number_input)
+
+what_number_input = int(input("what number do you want to someone guess?"))
+
+user_number = input("what number is that your body try to hide?")
+
+
+if number_to_guess_input = user number
+	print("you did!")
+
+else: 
+	user_number = int(input("try again"))
