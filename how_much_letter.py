ec1f9379268c601691f34f8b692736a6e495e473
kabp11 <56281549+kabp11@users.noreply.github.com>
2019-10-18 10:04:45 -0500

    Add files via upload

---
+++ how_much_letter.py
@@ -0,0 +1,12 @@
+how_much_letter = int(input¨("INSERT THE TEXT THAT YOU WHANT TO NOW HOW MUCH VOCALS HAVE")).upper
+
+vocals = ["A","E","I","O","U"]
+
+comas = [","]
+
+vocals_n = 0
+
+for vocals in how_much_letter
+	vocals_n += 1
+
+print(vocals_n)
