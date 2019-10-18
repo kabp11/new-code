e4ed8bd61f39a6165e7fe541948445cd9557c126
kabp11 <56281549+kabp11@users.noreply.github.com>
2019-10-17 21:12:51 -0500

    Add files via upload

--- number_to_guess.py
+++ number_to_guess.py
@@ -1,12 +1,16 @@
-number_to_guess_input = {}.format(what_number_input)
-
 what_number_input = int(input("what number do you want to someone guess?"))
+what_number_input2 = what_number_input
+user_number_input = int(input("what number is that your body try to hide?"))
 
-user_number = input("what number is that your body try to hide?")
+if user_number_input == what_number_input:
+    user_number_input = True
+else:
+    user_number_input = False
 
+while user_number_input == False:
+    print("try again")
+    try_again = int(input("what number is that your body try to hide?"))
+    if try_again == what_number_input2:
+        user_number_input = True
 
-if number_to_guess_input = user number
-	print("you did!")
-
-else: 
-	user_number = int(input("try again"))
+print("you win")
