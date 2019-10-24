01893987c3ce1a45249d6d908bb004be52b5cedc
kabp11 <56281549+kabp11@users.noreply.github.com>
2019-10-23 21:33:46 -0500

    Add files via upload

--- comer_helado.py
+++ comer_helado.py
@@ -1,27 +1,50 @@
 apetece_helado_input = input("te apetece un helado? (si/no): ").upper()
 tiene_dinero_input = input("tienes dinero para un helado? (si/no): ").upper()
+incorrecto = False
+incorrecto2 = False
+apetece_helado = None
+tiene_dinero = None
 
 if apetece_helado_input  == "SI":
     apetece_helado = True
 elif apetece_helado_input == "NO":
     apetece_helado = False
-    print("que aburrido")
 else:
-    print("solo quiero saber si o no")
+    incorrecto = True
 
-apetece_helado = apetece_helado_input =="SI"
-tiene_dinero = tiene_dinero_input =="SI"
-
-if tiene_dinero_input  == "SI":
+if tiene_dinero_input == "SI":
     tiene_dinero = True
 elif tiene_dinero_input == "NO":
     tiene_dinero = False
-    print("que triste")
 else:
-    print("solo quiero saber si o no")
+    incorrecto2 = True
 
+while incorrecto == True and incorrecto2 == True:
+    print("solamente si o no")
+    apetece_helado_input = input("te apetece un helado? (si/no): ").upper()
+    tiene_dinero_input = input("tienes dinero para un helado? (si/no): ").upper()
+    if apetece_helado_input == "SI":
+        apetece_helado = True
+        incorrecto = False
+    elif apetece_helado_input == "NO":
+        apetece_helado = False
+        incorrecto = False
+    else:
+        incorrecto = True
+    if tiene_dinero_input == "SI":
+        tiene_dinero = True
+        incorrecto = False
+    elif tiene_dinero_input == "NO":
+        tiene_dinero = False
+        incorrecto = False
+    else:
+        incorrecto2 = True
 
-if apetece_helado and tiene_dinero :
+if apetece_helado and tiene_dinero:
     print("pues comete un helado")
 elif apetece_helado == False and tiene_dinero == True:
     print("comprate otra cosa")
+elif apetece_helado == True and tiene_dinero == False:
+    print("Que triste")
+elif apetece_helado == False and tiene_dinero == False:
+    print("pues llegale")
